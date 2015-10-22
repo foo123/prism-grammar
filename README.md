3162d61cd2727c96fe01bf12e29027fee6ff2dba
Nikos M <nikosms0@gmail.com>
2015-10-22 23:35:19 +0300

    2.5.0-pre updates, edits, format sample grammars

--- README.md
+++ README.md
@@ -48,83 +48,75 @@ An example for XML:
 
 
 ```javascript
-
 // 1. a partial xml grammar in simple JSON format
 var xml_grammar = {
     
-    // prefix ID for regular expressions, represented as strings, used in the grammar
-    "RegExpID": "RE::",
+// prefix ID for regular expressions, represented as strings, used in the grammar
+"RegExpID"                          : "RE::",
 
-    // Style model
-    "Style": {
-        // lang token type  -> Prism (style) tag
-        "declaration"            : "tag",
-        "doctype"                : "entity",
-        "meta"                   : "entity",
-        "comment"                : "comment",
-        "cdata"                  : "comment",
-        "atom"                   : "string",
-        "open_tag"               : "tag",
-        "close_open_tag"         : "tag",
-        "auto_close_open_tag"    : "tag",
-        "close_tag"              : "tag",
-        "att"                    : "attr-name",
-        "number"                 : "number",
-        // "" represents default style or unstyled
-        "string"                 : "",
-        // allow block delims / interior to have different styles
-        "string.inside"          : "attr-value"
-    },
+// Style model
+"Style"                             : {
+    
+     "declaration"                  : "tag"
+    ,"doctype"                      : "entity"
+    ,"meta"                         : "entity"
+    ,"comment"                      : "comment"
+    ,"cdata"                        : "comment"
+    ,"atom"                         : "string"
+    ,"open_tag"                     : "tag"
+    ,"close_open_tag"               : "tag"
+    ,"auto_close_open_tag"          : "tag"
+    ,"close_tag"                    : "tag"
+    ,"attribute"                    : "attr-name"
+    ,"number"                       : "number"
+    // "" represents default style or unstyled
+    ,"string"                       : ""
+    // allow block delims / interior to have different styles
+    ,"string.inside"                : "attr-value"
 
-    // Lexical model
-    "Lex": {
-        "declaration:block": ["&lt;?xml","?>"],
-        "doctype:block": ["RE::/&lt;!doctype\\b/i",">"],
-        "meta:block": ["RE::/&lt;\\?[_a-zA-Z][\\w\\._\\-]*/","?>"],
-        "comment:comment": ["&lt;!--","-->"],
-        "cdata:block": ["&lt;![CDATA[","]]>"],
-        "string:line-block": [[ "\"" ],[ "'" ]],
-        "number": ["RE::/[0-9]\\d*/", "RE::/#[0-9a-fA-F]+/"],
-        "atom": ["RE::/&amp;#x[a-fA-F\\d]+;/", "RE::/&amp;#[\\d]+;/", "RE::/&amp;[a-zA-Z][a-zA-Z0-9]*;/"],
-        "att": "RE::/[_a-zA-Z][_a-zA-Z0-9\\-]*/",
-        "open_tag": "RE::/&lt;([_a-zA-Z][_a-zA-Z0-9\\-]*)/",
-        "close_open_tag": ">",
-        "auto_close_open_tag": "/>",
-        "close_tag": "RE::/&lt;\\/([_a-zA-Z][_a-zA-Z0-9\\-]*)>/",
-        "text": "RE::/[^&]+/",
-        
-        // actions
-        "ctx:action": {"context":true},
-        "\\ctx:action": {"context":false},
-        "unique:action": {
-            "unique": ["id", "$1"],
-            "msg": "Duplicate id value \"$0\""
-        },
-        "unique_att:action": {
-            "unique": ["att", "$0"],
-            "in-context":true,
-            "msg": "Duplicate attribute \"$0\""
-        },
-        "match:action": {"push":"<$1>","ci": true},
-        "\\match:action": {
-            "pop": "<$1>",
-            "ci": true,
-            "msg": "Tags \"$0\" and \"$1\" do not match"
-        },
-        "nomatch:action": {"pop":null},
-        "out_of_place:error": "\"$2$3\" can only be at the beginning of XML document"
-    },
+},
+
+// Lexical model
+"Lex"                               : {
+     
+     "comment:comment"              : ["&lt;!--", "-->"]
+    ,"declaration:block"            : ["&lt;?xml", "?>"]
+    ,"doctype:block"                : ["RE::/&lt;!doctype\\b/i", ">"]
+    ,"meta:block"                   : ["RE::/&lt;\\?[_a-zA-Z][\\w\\._\\-]*/", "?>"]
+    ,"cdata:block"                  : ["&lt;![CDATA[", "]]>"]
+    ,"open_tag"                     : "RE::/&lt;([_a-zA-Z][_a-zA-Z0-9\\-]*)/"
+    ,"close_tag"                    : "RE::/&lt;\\/([_a-zA-Z][_a-zA-Z0-9\\-]*)>/"
+    ,"attribute"                    : "RE::/[_a-zA-Z][_a-zA-Z0-9\\-]*/"
+    ,"string:line-block"            : [["\""], ["'"]]
+    ,"number"                       : ["RE::/[0-9]\\d*/", "RE::/#[0-9a-fA-F]+/"]
+    ,"atom"                         : ["RE::/&amp;#x[a-fA-F\\d]+;/", "RE::/&amp;#[\\d]+;/", "RE::/&amp;[a-zA-Z][a-zA-Z0-9]*;/"]
+    ,"text"                         : "RE::/[^&]+/"
     
-    // Syntax model (optional)
-    "Syntax": {
-        "tag_att": "'id'.att unique_att '=' string unique | att unique_att '=' (string | number)",
-        "start_tag": "open_tag match ctx tag_att* (close_open_tag | auto_close_open_tag nomatch) \\ctx",
-        "end_tag": "close_tag \\match",
-        "xml": "(^^1 declaration? doctype?) (declaration.error out_of_place | doctype.error out_of_place | comment | meta | cdata | start_tag | end_tag | atom | text)*"
-    },
+    // actions
+    ,"tag_ctx:action"               : {"context":true}
+    ,"\\tag_ctx:action"             : {"context":false}
+    ,"unique_id:action"             : {"unique":["xml", "$1"],"msg":"Duplicate id value \"$0\""}
+    ,"unique_att:action"            : {"unique":["tag", "$0"],"msg":"Duplicate attribute \"$0\"","in-context":true}
+    ,"tag_opened:action"            : {"push":"<$1>","ci":true}
+    ,"tag_closed:action"            : {"pop":"<$1>","ci":true,"msg":"Tags \"$0\" and \"$1\" do not match"}
+    ,"tag_autoclosed:action"        : {"pop":null}
+    ,"out_of_place:error"           : "\"$2$3\" can only be at the beginning of XML document"
     
-    // what to parse and in what order
-    "Parser": [ ["xml"] ]
+},
+    
+// Syntax model (optional)
+"Syntax"                            : {
+     
+     "tag_att"                      : "'id'.attribute unique_att '=' string unique_id | attribute unique_att '=' (string | number)"
+    ,"start_tag"                    : "open_tag tag_ctx tag_opened tag_att* ('>'.tag | '/>'.tag tag_autoclosed) \\tag_ctx"
+    ,"end_tag"                      : "close_tag tag_closed"
+    ,"xml"                          : "(^^1 declaration? doctype?) (declaration.error out_of_place | doctype.error out_of_place | comment | meta | cdata | start_tag | end_tag | atom | text)*"
+    
+},
+    
+// what to parse and in what order
+"Parser"                            : [ ["xml"] ]
+
 };
 
 // 2. parse the grammar into a Prism-compatible syntax-highlighter
--- test/grammars/javascript.js
+++ test/grammars/javascript.js
@@ -1,175 +1,123 @@
-// 1. an almost complete javascript grammar in simple JSON format
+// 1. a partial javascript grammar in simple JSON format
 var js_grammar = {
         
-    // prefix ID for regular expressions used in the grammar
-    "RegExpID": "RegExp::",
+// prefix ID for regular expressions used in the grammar
+"RegExpID"                          : "RegExp::",
     
-    // Style model
-    "Style": {
-        // lang token type  -> Prism (style) tag
-        "comment":    "comment",
-        "atom":       "entity",
-        "keyword":    "keyword",
-        "this":       "keyword",
-        "builtin":    "builtin",
-        "operator":   "operator",
-        "identifier": "symbol",
-        "property":   "property",
-        "number":     "number",
-        "string":     "string",
-        "regex":      "regex"
-    },
+// Style model
+"Style"                             : {
 
-    // Lexical model
-    "Lex": {
-        // comments
-        "comment" : {
-            "type" : "comment",
-            "tokens" : [
-                // line comment
-                // start, end delims  (null matches end-of-line)
-                [  "//",  null ],
-                // block comments
-                // start,  end    delims
-                [  "/*",   "*/" ]
-            ]
-        },
-        
-        // general identifiers
-        "identifier" : "RegExp::/[_A-Za-z$][_A-Za-z0-9$]*/",
-        
-        "this" : "RegExp::/this\\b/",
-        
-        "property" : "RegExp::/[_A-Za-z$][_A-Za-z0-9$]*/",
-        
-        // numbers, in order of matching
-        "number" : [
-            // floats
-            "RegExp::/\\d*\\.\\d+(e[\\+\\-]?\\d+)?/",
-            "RegExp::/\\d+\\.\\d*/",
-            "RegExp::/\\.\\d+/",
-            // integers
-            // hex
-            "RegExp::/0x[0-9a-fA-F]+L?/",
-            // binary
-            "RegExp::/0b[01]+L?/",
-            // octal
-            "RegExp::/0o[0-7]+L?/",
-            // decimal
-            "RegExp::/[1-9]\\d*(e[\\+\\-]?\\d+)?L?/",
-            // just zero
-            "RegExp::/0(?![\\dx])/"
-        ],
+     "comment"                      : "comment"
+    ,"atom"                         : "entity"
+    ,"keyword"                      : "keyword"
+    ,"this"                         : "keyword"
+    ,"builtin"                      : "builtin"
+    ,"operator"                     : "operator"
+    ,"identifier"                   : "symbol"
+    ,"property"                     : "property"
+    ,"number"                       : "number"
+    ,"string"                       : "string"
+    ,"regex"                        : "regex"
 
-        // usual strings
-        "string" : {
-            "type" : "escaped-block",
-            "escape" : "\\",
-            // start, end of string (can be the matched regex group ie. 1 )
-            "tokens" : [ "RegExp::/(['\"])/",   1 ]
-        },
-        
-        // literal regular expressions
-        "regex" : {
-            "type" : "escaped-block",
-            "escape" : "\\",
-            // javascript literal regular expressions can be parsed similar to strings
-            "tokens" : [ "/",    "RegExp::#/[gimy]{0,4}#" ]
-        },
-        
-        // operators
-        "operator" : {
-            "combine" : true,
-            "tokens" : [
-                "\\", "+", "-", "*", "/", "%", "&", "|", "^", "~", "<", ">" , "!",
-                "||", "&&", "==", "!=", "<=", ">=", "<>", ">>", "<<",
-                "===", "!==", "<<<", ">>>" 
-            ]
-        },
-        
-        // delimiters
-        "delimiter" : {
-            "combine" : true,
-            "tokens" : [
-                "(", ")", "[", "]", "{", "}", ",", "=", ";", "?", ":",
-                "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "++", "--",
-                ">>=", "<<="
-            ]
-        },
-            
-        // atoms
-        "atom" : {
-            // enable autocompletion for these tokens, with their associated token ID
-            "autocomplete" : true,
-            "tokens" : [
-                "true", "false", 
-                "null", "undefined", 
-                "NaN", "Infinity"
-            ]
-        },
+},
 
-        // keywords
-        "keyword" : {
-            // enable autocompletion for these tokens, with their associated token ID
-            "autocomplete" : true,
-            "tokens" : [ 
-                "if", "while", "with", "else", "do", "try", "finally",
-                "return", "break", "continue", "new", "delete", "throw",
-                "var", "const", "let", "function", "catch",
-                "for", "switch", "case", "default",
-                "in", "typeof", "instanceof"
-            ]
-        },
-        
-        // builtins
-        "builtin" : {
-            // enable autocompletion for these tokens, with their associated token ID
-            "autocomplete" : true,
-            "tokens" : [ 
-                "Object", "Array", "String", "Number", "RegExp", "Exception",
-                "setTimeout", "setInterval", "alert", "console"
-            ]
-        }
-    },
+// Lexical model
+"Lex"                               : {
+
+     "comment"                      : {"type":"comment","tokens":[
+                                    // line comment
+                                    // start, end delims  (null matches end-of-line)
+                                    [  "//",  null ],
+                                    // block comments
+                                    // start,  end    delims
+                                    [  "/*",   "*/" ]
+                                    ]}
+    ,"identifier"                   : "RegExp::/[_A-Za-z$][_A-Za-z0-9$]*/"
+    ,"this"                         : "RegExp::/this\\b/"
+    ,"property"                     : "RegExp::/[_A-Za-z$][_A-Za-z0-9$]*/"
+    ,"number"                       : [
+                                    // floats
+                                    "RegExp::/\\d*\\.\\d+(e[\\+\\-]?\\d+)?/",
+                                    "RegExp::/\\d+\\.\\d*/",
+                                    "RegExp::/\\.\\d+/",
+                                    // integers
+                                    // hex
+                                    "RegExp::/0x[0-9a-fA-F]+L?/",
+                                    // binary
+                                    "RegExp::/0b[01]+L?/",
+                                    // octal
+                                    "RegExp::/0o[0-7]+L?/",
+                                    // decimal
+                                    "RegExp::/[1-9]\\d*(e[\\+\\-]?\\d+)?L?/",
+                                    // just zero
+                                    "RegExp::/0(?![\\dx])/"
+                                    ]
+    ,"string"                       : {"type":"escaped-block","escape":"\\","tokens":
+                                    // start, end of string (can be the matched regex group ie. 1 )
+                                    [ "RegExp::/(['\"])/",   1 ]
+                                    }
+    ,"regex"                        : {"type":"escaped-block","escape":"\\","tokens":
+                                    // javascript literal regular expressions can be parsed similar to strings
+                                    [ "/",    "RegExp::#/[gimy]{0,4}#" ]
+                                    }
+    ,"operator"                     : {"tokens":[
+                                    "+", "-", "++", "--", "%", ">>", "<<", ">>>",
+                                    "*", "/", "^", "|", "&", "!", "~",
+                                    ">", "<", "<=", ">=", "!=", "!==",
+                                    "=", "==", "===", "+=", "-=", "%=",
+                                    ">>=", ">>>=", "<<=", "*=", "/=", "|=", "&="
+                                    ]}
+    ,"delimiter"                    : {"tokens":[
+                                    "(", ")", "[", "]", "{", "}", ",", "=", ";", "?", ":",
+                                    "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "++", "--",
+                                    ">>=", "<<="
+                                    ]}
+    ,"atom"                         : {"autocomplete":true,"tokens":[
+                                    "true", "false", 
+                                    "null", "undefined", 
+                                    "NaN", "Infinity"
+                                    ]}
+    ,"keyword"                      : {"autocomplete":true,"tokens":[ 
+                                    "if", "while", "with", "else", "do", "try", "finally",
+                                    "return", "break", "continue", "new", "delete", "throw",
+                                    "var", "const", "let", "function", "catch", "void",
+                                    "for", "switch", "case", "default", "class", "import", "yield",
+                                    "in", "typeof", "instanceof"
+                                    ]}
+    ,"builtin"                      : {"autocomplete":true,"tokens":[ 
+                                    "Object", "Function", "Array", "String", 
+                                    "Date", "Number", "RegExp", "Math", "Exception",
+                                    "setTimeout", "setInterval", "parseInt", "parseFloat", 
+                                    "isFinite", "isNan", "alert", "prompt", "console", 
+                                    "window", "global", "this"
+                                    ]}
     
-    // Syntax model (optional)
-    "Syntax": {
-        
-        "dotProperty" : {
-            "type" : "group",
-            "match" : "all",
-            "tokens" : [ ".", "property" ]
-        },
-        
-        "builtinOrIdentifier" : {
-            "type" : "group",
-            "match" : "either",
-            "tokens" : [ "}", ")", "this", "builtin", "identifier", "dotProperty" ]
-        },
-        
-        "dotProperties" : {
-            "type" : "group",
-            "match" : "zeroOrMore",
-            "tokens" : [ "dotProperty" ]
-        },
-        
-        "builtinOrIdentifierWithProperties" : {
-            "type" : "n-gram",
-            "tokens" : [
-                [ "builtinOrIdentifier", "dotProperties" ]
-            ]
-        }
-    },
+},
+    
+// Syntax model (optional)
+"Syntax"                            : {
 
-    // what to parse and in what order
-    "Parser": [
-        "comment",
-        "number",
-        "string",
-        "regex",
-        "keyword",
-        "operator",
-        "atom",
-        "builtinOrIdentifierWithProperties"
-    ]
+     "dotProperty"                  : {"type":"group","match":"all","tokens":[ ".", "property" ]}
+    ,"builtinOrIdentifier"          : {"type":"either","tokens":[
+                                    "}", ")", "this", "builtin", "identifier", "dotProperty"
+                                    ]}
+    ,"dotProperties"                : {"zeroOrMore": ["dotProperty"]}
+    ,"identifierWithProperties"     : {"type":"n-gram","tokens":[
+                                    [ "builtinOrIdentifier", "dotProperties" ]
+                                    ]}
+
+},
+
+// what to parse and in what order
+"Parser"                            : [
+                                    "comment",
+                                    "number",
+                                    "string",
+                                    "regex",
+                                    "keyword",
+                                    "operator",
+                                    "atom",
+                                    "identifierWithProperties"
+                                    ]
+
 };
