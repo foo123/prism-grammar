1e5bc765618d6a359354cf462d613134c13b7f9f
Nikos M <nikosms0@gmail.com>
2016-04-09 22:32:58 +0300

    v.4.0.0 in progress

--- src/main.js
+++ src/main.js
@@ -10,6 +10,43 @@
 **/
 
 
+var 
+isNode = !!(("undefined" !== typeof global) && ("[object global]" === toString.call(global))),
+isBrowser = !!(!isNode && ("undefined" !== typeof navigator)),
+isWorker = !!(isBrowser && ("function" === typeof importScripts) && (navigator instanceof WorkerNavigator)),
+this_path = (function(isNode, isBrowser, isWorker) {
+    // Get current filename/path
+    var file = null, path = null, base = null, scripts;
+    if ( isNode ) 
+    {
+        // http://nodejs.org/docs/latest/api/globals.html#globals_filename
+        // this should hold the current file in node
+        file = __filename; path = __dirname; base = __dirname;
+    }
+    else if ( isWorker )
+    {
+        // https://developer.mozilla.org/en-US/docs/Web/API/WorkerLocation
+        // this should hold the current url in a web worker
+        file = self.location.href; path = file.split('/').slice(0, -1).join('/');
+    }
+    else if ( isBrowser )
+    {
+        // get last script (should be the current one) in browser
+        base = document.location.href.split('#')[0].split('?')[0].split('/').slice(0, -1).join('/');
+        if ((scripts = document.getElementsByTagName('script')) && scripts.length)
+        {
+            file = scripts[scripts.length - 1].src;
+            path = file.split('/').slice(0, -1).join('/');
+        }
+    }
+    return { path: path, file: file, base: base };
+})(isNode, isBrowser, isWorker),
+
+// browser caches worker source file, even with reset/reload, try to not cache
+NOCACHE = '?nocache=' + uuid('nonce') + '_' + (~~(1000*Math.random( ))),
+
+$Prism$ = 'undefined' !== typeof Prism ? Prism : null;
+
 //
 // parser factories
 var PrismParser = Class(Parser, {
@@ -21,17 +58,17 @@ var PrismParser = Class(Parser, {
         self.ERR = /*grammar.Style.error ||*/ self.$ERR;
     }
     
-    ,tokenize: function( stream, state, row ) {
+    ,tokenize: function( stream, mode, row ) {
         var self = this, tokens = [], token, buf = [], id = null,
             raw_content = function( token ) { return token.content; },
             maybe_raw = function( token ) { return self.$DEF === token.type ? token.content : token; }
         ;
-        //state.line = row || 0;
-        if ( stream.eol() ) { state.line++; if ( state.$blank$ ) state.bline++; }
+        //mode.state.line = row || 0;
+        if ( stream.eol() ) { mode.state.line++; if ( mode.state.$blank$ ) mode.state.bline++; }
         else while ( !stream.eol() )
         {
-            token = self.token( stream, state );
-            if ( state.$actionerr$ )
+            token = mode.parser.get( stream, mode );
+            if ( mode.state.$actionerr$ )
             {
                 if ( buf.length ) tokens = tokens.concat( map( buf, raw_content ) );
                 tokens.push( token.content );
@@ -62,15 +99,71 @@ function esc_token( i, tokens )
     tokens[i] = t;
 }
 
+if ( isWorker ) onmessage = grammar_worker;
+
+// worker factory
+function grammar_worker( e )
+{
+    var msg = e ? e.data : null;
+    if ( !msg ) return;
+    if ( msg.init ) 
+    {        
+        var parser = grammar_worker.parser = new PrismGrammar.Parser( parse_grammar( msg.grammar ) );
+        if ( msg.subgrammars )
+        {
+            var langs = Object.keys(msg.subgrammars);
+            for(var i=0,l=langs.length; i<l; i++)
+                parser.subparser( langs[i], new PrismGrammar.Parser( parse_grammar( msg.subgrammars[langs[i]] ) ) );
+        }
+    }
+    else if ( msg.tokenize )
+    {
+        if ( grammar_worker.parser )
+            postMessage( {tokenize:1, tokens:grammar_worker.parser.parse(msg.code, TOKENS|ERRORS|FLAT).tokens} );
+    }
+}
+grammar_worker.parser = null;
+
+function WorkerMirror( grammar, subgrammars )
+{
+    var self = this, worker = null, pending = [];
+    worker = new Worker( this_path.file );
+    worker.postMessage({init:1, grammar:grammar, subgrammars:subgrammars});
+    worker.onMessage = function( e ) {
+        if ( !pending || !pending.length ) return;
+        var msg = e.data || {}, cb;
+        if ( msg.tokenize )
+        {
+            cb = pending.shift( );
+            cb( msg.tokens );
+        }
+    };
+    
+    self.tokenize = function( code, cb ) {
+        if ( !worker ) return cb( null );
+        pending.push( cb );
+        worker.postMessage({tokenize:1, code:code});
+    };
+    
+    self.dispose = function( ) {
+        pending = null;
+        if ( worker ) worker.terminate( );
+        worker = null;
+        return self;
+    };
+}
+
 function get_mode( grammar, Prism ) 
 {
-    var prism_highlighter, is_hooked = 0, $Prism$ = Prism,
+    Prism = Prism || $Prism$; // pass Prism reference if not already available
+    
+    var PrismHighlighter, is_hooked = 0,
     
     highlighter$ = {
         'before-highlight': function( env ) {
             // use the custom parser for the grammar to highlight
             // hook only if the language matches
-            if ( prism_highlighter.$parser && (prism_highlighter.$lang === env.language) )
+            if ( PrismHighlighter.$parser && (PrismHighlighter.language === env.language) )
             {
                 // avoid double highlight work, set code to ""
                 env._code = env.code;
@@ -79,58 +172,78 @@ function get_mode( grammar, Prism )
         },
         
         'before-insert': function( env ) {
-            if ( prism_highlighter.$parser && (prism_highlighter.$lang === env.language) )
+            if ( PrismHighlighter.$parser && (PrismHighlighter.language === env.language) )
             {
                 // re-set
                 env.code = env._code;
                 env._code = "";
-                // tokenize code and transform to prism-compatible tokens
-                var tokens = prism_highlighter.$parser.parse(env.code, TOKENS|ERRORS|FLAT).tokens;
-                // html-escape code
-                if ( prism_highlighter.escapeHtml ) iterate( esc_token, 0, tokens.length-1, tokens );
-                env.highlightedCode = $Prism$.Token.stringify( tokens, env.language );
+                env.highlightedCode = Prism.Token.stringify(
+                    PrismHighlighter.tokenize( env.code, PrismHighlighter.escapeHtml ),
+                    env.language
+                );
             }
         }
     };
     
     // return a plugin that can be hooked-unhooked to Prism under certain language conditions
-    prism_highlighter = {
-        $id: uuid("prism_grammar_highlighter")
+    PrismHighlighter = {
+         $id: uuid("prism_grammar_highlighter")
         
+        ,$grammar: grammar
         ,$parser: new PrismGrammar.Parser( parse_grammar( grammar ) )
-        
-        ,$lang: null
-        
+        ,$worker: null
+        ,language: null
         // have escapeHtml flag true by default
         ,escapeHtml: true
         
-        // TODO:  a way to highlight in worker (like default prism async flag)
-        // post a request to prism repository?????
-        ,$async: false
+        ,tokenize: function( code, escapeHtml, workerCb ) {
+            var self = this;
+            if ( 'function' === typeof workerCb )
+            {
+                // use async tokenization in worker
+                if ( !self.$worker ) self.$worker = new WorkerMirror( self.$grammar, self.getSubgrammars() );
+                self.$worker.tokenize( code, function( tokens ){
+                    // html-escape code
+                    if ( tokens && escapeHtml ) iterate( esc_token, 0, tokens.length-1, tokens );
+                    workerCb( tokens );
+                });
+            }
+            else
+            {
+                // tokenize code and transform to prism-compatible tokens
+                var tokens = self.$parser.parse(code, TOKENS|ERRORS|FLAT).tokens;
+                // html-escape code
+                if ( escapeHtml ) iterate( esc_token, 0, tokens.length-1, tokens );
+                return tokens;
+            }
+        }
         
-        ,hook: function( language, Prism ) {
-            if ( is_hooked ) prism_highlighter.unhook();
-            if ( T_STR & get_type(Prism) )
+        ,hook: function( language, prism ) {
+            var self = this;
+            if ( is_hooked ) self.unhook();
+            if ( T_STR & get_type(prism) )
             {
                 // arguments given in different order
                 var tmp = language;
-                language = Prism;
-                Prism = tmp;
+                language = prism;
+                prism = tmp;
             }
-            $Prism$ = Prism || $Prism$;
-            prism_highlighter.$lang = language;
+            Prism = prism || Prism;
+            self.language = language;
             for (var hook in highlighter$ )
             {
                 if ( highlighter$[HAS](hook) )
-                    $Prism$.hooks.add( hook, highlighter$[hook] );
+                    Prism.hooks.add( hook, highlighter$[hook] );
             }
             is_hooked = 1;
         }
         
-        ,unhook: function( ) {
+        ,unhook: function( prism ) {
+            var self = this;
+            Prism = prism || Prism;
             if ( is_hooked )
             {
-                var prism_hooks = $Prism$.hooks.all, hook, this_hook;
+                var prism_hooks = Prism.hooks.all, hook, this_hook;
                 
                 for (hook in highlighter$)
                 {
@@ -141,19 +254,33 @@ function get_mode( grammar, Prism )
                     }
                 }
                 is_hooked = 0;
-                prism_highlighter.$lang = null;
-                $Prism$ = null;
+                self.language = null;
+                //Prism = null;
             }
         }
         
+        ,getSubgrammars: function( ) {
+            var self = this, parser = self.$parser,
+                langs = Object.keys(parser.$subgrammars),
+                subgrammars = {};
+            for(var i=0,l=langs.length; i<l; i++) subgrammars[langs[i]] = parser.$subgrammars[langs[i]].Mode.$grammar;
+            return subgrammars;
+        }
+        ,submode: function( lang, mode ) {
+            this.$parser.subparser( lang, mode.$parser );
+        }
+        
         ,dispose: function( ) {
-            prism_highlighter.unhook();
-            if ( prism_highlighter.$parser ) prism_highlighter.$parser.dispose( );
-            prism_highlighter.$parser = null;
-            prism_highlighter.$lang = null;
+            var self = this;
+            self.unhook( );
+            if ( self.$worker ) self.$worker.dispose( );
+            if ( self.$parser ) self.$parser.dispose( );
+            self.$parser = self.$worker = self.language = Prism = null;
         }
     };
-    return prism_highlighter;
+    // store a reference to Mode here
+    PrismHighlighter.$parser.Mode = PrismHighlighter;
+    return PrismHighlighter;
 }
 
 
