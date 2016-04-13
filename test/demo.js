41ecfe053ce8ce305e3fd9efc13b0eadd269da73
Nikos M <nikosms0@gmail.com>
2016-04-13 03:12:35 +0300

    v.4.0.0 (pre-release)
    * fix issue with block tokens up-to-line-end
    * fix issue with interleaved outer tokens inside block tokens (subgrammars ..)
    * parser changes to handle interleaved and outer tokens correctly
    * new block token property "empty" (default true), to enable a block token to be empty or not
    * better error recovery and error message handling
    * fix an issue with block tokens not displayed correctly in action errors
    * generic code matching
    * generic code folding (more options)
    * support sub-grammars / sub-parsers multiplexing (e.g html with javascript and css)
    * various changes and optimisations

--- test/demo.js
+++ test/demo.js
@@ -1,9 +1,23 @@
-function prism_grammar_demo(code, lang, grammar)
+function prism_grammar_demo(code, langs)
 {
     document.getElementById('editor-version').innerHTML = '1.2.0';
     document.getElementById('grammar-version').innerHTML = PrismGrammar.VERSION;
-    var mode = PrismGrammar.getMode( grammar );
-    mode.escapeHtml = true; // whether to escape html code to output properly, Prism.version 1.2.0+
-    mode.hook( lang, Prism );
+    var main_lang, main_mode;
+    for(var i=0,l=langs.length; i<l; i++)
+    {
+        if ( 0 === i )
+        {
+            // main mode
+            main_lang = langs[i].language;
+            main_mode = PrismGrammar.getMode( langs[i].grammar );
+        }
+        else
+        {
+            // submodes
+            main_mode.submode(langs[i].language, PrismGrammar.getMode( langs[i].grammar ));
+        }
+    }
+    main_mode.escapeHtml = true; // whether to escape html code to output properly, Prism.version 1.2.0+
+    main_mode.hook( main_lang, Prism );
     Prism.highlightElement( code );
 }
